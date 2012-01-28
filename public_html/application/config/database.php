97d1bf085c3d657ff2d6b60c98a6201efb96b05e
Eduardo Chiaro <e.chiaro@gmail.com>
2012-01-28 18:43:40 +0000

    db user

--- public_html/application/config/database.php
+++ public_html/application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['username'] = DB_USER;
+$db['default']['password'] = DB_PASSWORD;
 $db['default']['database'] = 'steamlife';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
