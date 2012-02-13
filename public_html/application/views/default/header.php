4f37de810884945819f072446ee6964610f8ed7f
Eduardo Chiaro <e.chiaro@gmail.com>
2012-02-13 16:28:07 +0000

    add style

--- public_html/application/views/default/header.php
+++ public_html/application/views/default/header.php
@@ -3,5 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>Steamlife</title>
+<link rel="stylesheet" type="text/css" href="<?php echo site_url('static/css/reset.css');?>">
+<link rel="stylesheet" type="text/css" href="<?php echo site_url('static/css/structure.css');?>">
 </head>
 <body>
--- public_html/application/views/login/form.php
+++ public_html/application/views/login/form.php
@@ -1,15 +1,38 @@
 <?php $this->load->view('default/header'); ?>
-<div id="login_form">
+
 	<h1>Steamlife Login</h1>
+	
+	
 	<?php if(isset($error)):?><div class="error"><?php echo $error ?></div><?php endif;?>
+	
+	<form class="box login">
+	<fieldset class="boxBody">
+	  <label>Username</label>
+	  <input type="text" tabindex="1" placeholder="PremiumPixel" required>
+	  <label><a href="#" class="rLink" tabindex="5">Forget your password?</a>Password</label>
+	  <input type="password" tabindex="2" required>
+	</fieldset>
+	<footer>
+	  <label><input type="checkbox" tabindex="3">Keep me logged in</label>
+	  <input type="submit" class="btnLogin" value="Login" tabindex="4">
+	</footer>
+</form>
     <?php 
-	echo form_open('login');
-	echo form_input('username', 'Username');
-	echo form_password('password', 'Password');
-	echo form_submit('submit', 'Login');
+	echo form_open('login',array('class' => 'box login'));
+	
+	echo form_fieldset('',array('class' => 'boxBody'));
+	echo form_label('Email');
+	echo form_input('username', '', array('tabindex'=>1, 'placeholder'=>'steam@thedeveloperinside.com', 'required'=>1));
+
+	
+	echo form_label('Password');
+	echo form_password('password', '', array('tabindex'=>3, 'required'=>1));
+	echo form_fieldset_close(); 
+	echo '<footer>';
+	echo form_button(array('type'=>'submit','content'=>'Log in','class' => 'btnLogin'));
+	echo '</footer>';
 	echo form_close();
 	?>
 
-</div>
 
 <?php $this->load->view('default/footer'); ?>
