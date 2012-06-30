b1fae6a6ef110f6ea72b505702eebf608f8cb161
Eduardo Chiaro <e.chiaro@gmail.com>
2012-06-30 16:31:57 +0100

    fix

--- application/config/database.php
+++ application/config/database.php
@@ -48,9 +48,9 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = "steamlife.mysql.fluxflex.com";
-$db['default']['username'] = "steamlife";
-$db['default']['password'] = "Zt2UCCRwoaN";
+$db['default']['hostname'] = "localhost";
+$db['default']['username'] = "root";
+$db['default']['password'] = "";
 $db['default']['database'] = 'steamlife';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
